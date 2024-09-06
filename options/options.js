2790069961ddf910c620f16e9371fc5a8009d991
Crowdbotics <team@crowdbotics.com>
2024-09-06 14:23:39 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "rough-voice-49316",
+  url: "https://rough-voice-49316.botics.co",
+  api: "https://rough-voice-49316.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
